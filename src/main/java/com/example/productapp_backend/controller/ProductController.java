077f8be727a02ffdb7b59ef8f4a86c174ef905f8
shajn786 <shajn786@gmail.com>
2022-12-05 17:34:31 +0530

    add

--- src/main/java/com/example/productapp_backend/controller/ProductController.java
+++ src/main/java/com/example/productapp_backend/controller/ProductController.java
@@ -1,12 +1,17 @@
 package com.example.productapp_backend.controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
-
+    @PostMapping("/")
+     public String Add()
+     {
+         return "add page";
+     }
 
 
 
